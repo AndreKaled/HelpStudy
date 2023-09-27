5b0c55f2f3333000b844463c3683ee07931474ee
Manfred Veiga <2021321227@ifam.edu.br>
2023-09-27 15:08:51 -0400

    termino de implementação dos pop-up (flashcards, listas e tarefas)

--- app/src/main/java/com/example/helpstudy/view/ListaFlashCardAdapter.java
+++ app/src/main/java/com/example/helpstudy/view/ListaFlashCardAdapter.java
@@ -2,14 +2,19 @@ package com.example.helpstudy.view;
 
 import android.content.Context;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.PopupMenu;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.helpstudy.controller.ControllerFlashCard;
 import com.example.helpstudy.R;
 import com.example.helpstudy.model.FlashCard;
+import com.example.helpstudy.utils.ROOT;
 
 import java.util.List;
 
@@ -42,13 +47,45 @@ public class ListaFlashCardAdapter extends BaseAdapter {
     public View getView(int position, View view, ViewGroup parent) {
         View v = LayoutInflater.from(context).inflate(R.layout.item_lista_flashcards, parent, false);
 
+        ControllerFlashCard controllerFlashCard = ControllerFlashCard.getInstancia();
         TextView textView_titulo = v.findViewById(R.id.tituloCard);
         TextView textViewDescricao = v.findViewById(R.id.descricaoCard);
-
+        Button bt = v.findViewById(R.id.menu_kebad);
         FlashCard flashCard = flashCards.get(position);
         textView_titulo.setText(flashCard.getTitulo());
         textViewDescricao.setText(flashCard.getDescricao());
 
+        bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                PopupMenu popupMenu = new PopupMenu(bt.getContext(), bt);
+
+                popupMenu.getMenuInflater().inflate(R.menu.menu_popup, popupMenu.getMenu());
+
+                popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem menuItem) {
+
+                        if(menuItem.getItemId() == R.id.editar){
+
+                            Toast.makeText(bt.getContext(), "Editar", Toast.LENGTH_SHORT).show();
+
+                        } else if (menuItem.getItemId() == R.id.deletar){
+
+                            Toast.makeText(bt.getContext(), "Deletar", Toast.LENGTH_SHORT).show();
+
+                        } else{
+
+                            Toast.makeText(bt.getContext(), "Erro", Toast.LENGTH_SHORT).show();
+                        }
+                        return true;
+                    }
+                });
+
+                popupMenu.show();
+            }
+        });
         return v;
     }
 }
--- app/src/main/java/com/example/helpstudy/view/TarefaAdapter.java
+++ app/src/main/java/com/example/helpstudy/view/TarefaAdapter.java
@@ -2,11 +2,15 @@ package com.example.helpstudy.view;
 
 import android.content.Context;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.PopupMenu;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.helpstudy.R;
 import com.example.helpstudy.controller.ControllerTarefas;
@@ -46,10 +50,42 @@ public class TarefaAdapter extends BaseAdapter {
 
         TextView textView_nome = v.findViewById(R.id.tituloTarefa);
         TextView textView_desc = v.findViewById(R.id.descricaoTarefa);
-
         Tarefa tarefa = list.get(position);
         textView_nome.setText(tarefa.getNome());
         textView_desc.setText(tarefa.getDescricao());
+        Button bt = v.findViewById(R.id.menu_kebad);
+
+        bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                PopupMenu popupMenu = new PopupMenu(bt.getContext(), bt);
+
+                popupMenu.getMenuInflater().inflate(R.menu.menu_popup, popupMenu.getMenu());
+
+                popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem menuItem) {
+
+                        if(menuItem.getItemId() == R.id.editar){
+
+                            Toast.makeText(bt.getContext(), "Editar", Toast.LENGTH_SHORT).show();
+
+                        } else if (menuItem.getItemId() == R.id.deletar){
+
+                            Toast.makeText(bt.getContext(), "Deletar", Toast.LENGTH_SHORT).show();
+
+                        } else{
+
+                            Toast.makeText(bt.getContext(), "Erro", Toast.LENGTH_SHORT).show();
+                        }
+                        return true;
+                    }
+                });
+
+                popupMenu.show();
+            }
+        });
 
         return v;
     }
