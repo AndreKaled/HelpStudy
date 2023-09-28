a054212e0ccdc2aa37d063437383c7d610cf4bef
Andrezinho <andrekaled9@gmail.com>
2023-09-28 17:57:38 -0400

    criando banco de dados locais

---
+++ app/src/main/java/com/example/helpstudy/datasource/DataLocal.java
@@ -0,0 +1,54 @@
+package com.example.helpstudy.datasource;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DataLocal extends SQLiteOpenHelper {
+
+    private static final String NAME_DATA_LOCAL = "HelpStudy";
+    private static final int VERSION = 1;
+    public static final String TABELA_FLASHCARDS = "Flashcards";
+    public static final String TITULO_FLASHCARD = "tituloFlashcard", DESCRICAO_FLASHCARD = "descricaoFlashcard", ID_FLASHCARD = "idFlashcard";
+    public static final String TABELA_LISTAS ="Listas";
+    public static final String TITULO_LISTA = "tituloLista", ID_LISTA = "idLista";
+    public static final String TABELA_TAREFAS = "Tarefas";
+    public static final String ID_TAREFA = "idTarefa", NOME_TAREFA = "tituloTarefa", DESCRICAO_TAREFA = "descricaoTarefa", CHAVE_ESTRANGEIRA_TAREFA = "lista",
+    DATA_ENTREGA_TAREFA = "dataEntrega", CONCLUIDA_TAREFA = "concluida";
+
+
+    public DataLocal(Context context) {
+        super(context, NAME_DATA_LOCAL, null, VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        String SQL_FLASHCARDS = " CREATE TABLE " +TABELA_FLASHCARDS +"(" +
+                ID_FLASHCARD +" INTEGER PRIMARY KEY AUTOINCREMENT," +
+                TITULO_FLASHCARD +" TEXT NOT NULL," +
+                DESCRICAO_FLASHCARD +" TEXT );";
+
+        String SQL_LISTAS = " CREATE TABLE " +TABELA_LISTAS +"(" +
+                ID_LISTA +" INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                TITULO_LISTA +" TEXT NOT NULL);";
+
+        String SQL_TAREFAS = " CREATE TABLE " +TABELA_TAREFAS +"(" +
+                ID_TAREFA +" INTEGER PRIMARY KEY AUTOINCREMENT," +
+                NOME_TAREFA +" TEXT NOT NULL," +
+                DESCRICAO_TAREFA +" TEXT," +
+                DATA_ENTREGA_TAREFA +" TEXT, " +
+                CONCLUIDA_TAREFA +" INTEGER NOT NULL CHECK(" +CONCLUIDA_TAREFA +" = 0 OR " +CONCLUIDA_TAREFA +" = 1)," +
+                CHAVE_ESTRANGEIRA_TAREFA +" INTEGER," +
+                " FOREIGN KEY(" +CHAVE_ESTRANGEIRA_TAREFA +") REFERENCES " +TABELA_LISTAS+"(" +ID_LISTA +"));";
+
+
+        sqLiteDatabase.execSQL(SQL_FLASHCARDS);
+        sqLiteDatabase.execSQL(SQL_LISTAS);
+        sqLiteDatabase.execSQL(SQL_TAREFAS);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
