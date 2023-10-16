5ae7a4e39793097f301d8ab5ba011c0f93e2c2b5
Manfred Veiga <2021321227@ifam.edu.br>
2023-10-16 17:53:50 -0400

    inicio crud usuario

--- app/src/main/java/com/example/helpstudy/view/PerfilFragments.java
+++ app/src/main/java/com/example/helpstudy/view/PerfilFragments.java
@@ -17,7 +17,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.helpstudy.R;
 import com.example.helpstudy.datasource.DataSource;
@@ -31,15 +33,33 @@ public class PerfilFragments extends Fragment {
     Preferencias pref;
     Intent intent;
 
+    Boolean apertado;
+    EditText editTextNome, editTextEmail, editTextSenha;
+
+    TextView textNome,textEmail, textSenha, emailPerfil, nomePerfil, senhaPerfil;
+
+    Button btEditar;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
         View rootView = inflater.inflate(R.layout.fragment_conquistas, container, false);
 
+        apertado = true;
         btBackup = rootView.findViewById(R.id.buttonTesteBackup);
         btSair = rootView.findViewById(R.id.sairBt);
+        btEditar = rootView.findViewById(R.id.btEditar);
         pref = new Preferencias(getContext());
+        textNome = rootView.findViewById(R.id.editNome);
+        editTextNome = rootView.findViewById(R.id.editTextNome);
+        textEmail = rootView.findViewById(R.id.emailPerfil);
+        editTextEmail = rootView.findViewById(R.id.editTextEmail);
+        textSenha  = rootView.findViewById(R.id.editSenha);
+        editTextSenha = rootView.findViewById(R.id.editTextSenha);
+        nomePerfil = rootView.findViewById(R.id.nomePerfil);
+        emailPerfil = rootView.findViewById(R.id.emailPerfil);
+        senhaPerfil = rootView.findViewById(R.id.senhaPerfil);
         btBackup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -60,6 +80,35 @@ public class PerfilFragments extends Fragment {
                 intent = new Intent();
                 intent.setClass(getActivity(), TelaLogin.class);
                 getActivity().startActivity(intent);
+            }
+        });
+
+        btEditar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if(apertado){
+
+                    textNome.setVisibility(View.VISIBLE);
+                    textEmail.setVisibility(View.VISIBLE);
+                    textSenha.setVisibility(View.VISIBLE);
+                    editTextEmail.setVisibility(View.INVISIBLE);
+                    editTextNome.setVisibility(View.INVISIBLE);
+                    editTextSenha.setVisibility((View.INVISIBLE));
+                    apertado = false;
+
+                }else{
+
+                    textNome.setVisibility(View.INVISIBLE);
+                    textEmail.setVisibility(View.INVISIBLE);
+                    textSenha.setVisibility(View.INVISIBLE);
+                    editTextEmail.setVisibility(View.VISIBLE);
+                    editTextNome.setVisibility(View.VISIBLE);
+                    editTextSenha.setVisibility((View.VISIBLE));
+                    apertado = true;
+
+
+                }
             }
         });
         // Inflate the layout for this fragment
