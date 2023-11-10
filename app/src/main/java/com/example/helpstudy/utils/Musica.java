a0fb0ec6566d76e93cb66ff9eca7e20d183eaf17
Andrezinho <andrekaled9@gmail.com>
2023-11-10 15:19:52 -0400

    nao sei oq fiz mas deu certo pra musica

--- app/src/main/java/com/example/helpstudy/utils/Musica.java
+++ app/src/main/java/com/example/helpstudy/utils/Musica.java
@@ -7,48 +7,33 @@ import android.provider.MediaStore;
 import com.example.helpstudy.R;
 
 public class Musica {
-
-
-    Context context;
-    public static MediaPlayer mediaPlayer;
+    static Context context;
+    public static MediaPlayer mediaPlayer = null;
 
     public Musica(Context context){
-
-
-        this.context = context;
-        this.mediaPlayer =(MediaPlayer) MediaPlayer.create(context, R.raw.som);
+        if(mediaPlayer == null) {
+            this.context = context;
+            this.mediaPlayer = (MediaPlayer) MediaPlayer.create(context, R.raw.som);
+        }
     }
     public void startMusic(){
-
-                mediaPlayer = (MediaPlayer) MediaPlayer.create(context, R.raw.som);
-
-
-            if(mediaPlayer.isPlaying() == false){
-
-                    mediaPlayer.start();
-            }
+        if(mediaPlayer.isPlaying() == false){
+            mediaPlayer = (MediaPlayer) MediaPlayer.create(context, R.raw.som);
+            mediaPlayer.start();
+        }
     }
-
-
     public Boolean startPause(){
-
         if(mediaPlayer.isPlaying() == true){
-
             pauseMusic();
             return true;
         } else {
-
-
             startMusic();
             return false;
         }
     }
     public void pauseMusic(){
-
         if(mediaPlayer.isPlaying()== true){
-
             mediaPlayer.release();
-            mediaPlayer = null;
             mediaPlayer = (MediaPlayer) MediaPlayer.create(context, R.raw.som);
         }
     }
